bb3021077d273e13ee000c559d81ddb95812cd63
Michael Linford <linford.mike@gmail.com>
2021-01-11 16:24:05 +0000

    Accounted for extra cases in Statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,7 +3,7 @@
 require 'statement'
 
 describe Statement do
-  let(:account) do
+  let(:account1) do
     double :account, history: {
       date: ['10/01/2012', '13/01/2012', '14/01/2012'],
       credit: [100_000, 200_000, 0],
@@ -11,13 +11,31 @@ describe Statement do
       balance: [100_000, 300_000, 250_000]
     }
   end
-  subject(:test_statement) { Statement.new(account: account) }
+
+  let(:account2) do
+    double :account, history: {
+      date: ['31/01/2020', '25/09/2020'],
+      credit: [10_000, 20_000],
+      debit: [0, 0],
+      balance: [10_000, 30_000]
+    }
+  end
+  subject(:test_statement1) { Statement.new(account: account1) }
+  subject(:test_statement2) { Statement.new(account: account2) }
   it 'Will have the correct output according to the acceptance criterea.' do
-    expect(test_statement.display_statement).to eq([
+    expect(test_statement1.display_statement).to eq([
       'date || credit || debit || balance',
       '14/01/2012 || || 500.00 || 2500.00',
       '13/01/2012 || 2000.00 || || 3000.00',
       '10/01/2012 || 1000.00 || || 1000.00'
+    ].join("\n"))
+  end
+
+  it 'Will have the correct output for account2' do
+    expect(test_statement2.display_statement).to eq([
+      'date || credit || debit || balance',
+      '25/09/2020 || 200.00 || || 300.00',
+      '31/01/2020 || 100.00 || || 100.00'
     ].join("\n"))
   end
 end
